7ba71c6bec200e9c7f32aabd1f8c5c763df42c46
glemma32 <genet.woldeyes@gmail.com>
2017-05-25 11:42:49 -0400

    first commit

---
+++ MathTwo.java
@@ -0,0 +1,11 @@
+
+public class MathTwo 
+{
+	public static void main(String[] args) 
+	{
+		int mynumber = 2;
+		double myothernumber = 1.7938;
+		System.out.println(mynumber);
+		System.out.println(myothernumber);
+	}	
+}
